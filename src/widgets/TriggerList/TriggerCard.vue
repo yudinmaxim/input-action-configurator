4160f6c78face5a497aa63fc0e3188a4b6e53cfa
Maxim Yudin <maxim.yudin@smartcom.city>
2026-04-18 23:39:09 +0300

    поправил стили копировани триггера

--- src/widgets/TriggerList/TriggerCard.vue
+++ src/widgets/TriggerList/TriggerCard.vue
@@ -40,7 +40,7 @@ const showConfirm = ref(false)
           {{ props.trigger.type }}
         </div>
       </div>
-      <BaseIconButton variant="icon" @click.stop="emit('duplicate')" title="Дублировать">
+      <BaseIconButton variant="default" @click.stop="emit('duplicate')" title="Дублировать">
         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
           <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1"/>
